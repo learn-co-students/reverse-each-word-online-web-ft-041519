89f7c4430f1c88af77bb44a2e2815e2e61a0aa68
Annia Forney <anniaforney@gmail.com>
2019-04-20 19:51:56 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,14 @@
+def reverse_each_word(string)
+  reverse_each_word ("Hi")
+  return string 
+end 
+
+def reverse_each_word(string two)
+  reverse_each_word ("Pizza")
+  string two.each 
+  return string
+end 
+
+def reverse_each_word(string)
+  string.collect 
+end 
